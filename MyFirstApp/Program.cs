d27c6e7eeee738367f19221dc09ad122d0597e89
LaurMaldis <117661481+LaurMaldis@users.noreply.github.com>
2023-09-07 23:00:22 +0300

    Initial commit

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,14 @@
+﻿// See on minu ilus koodike, kommentaar 1
+Console.WriteLine("Enter your name:");
+
+//string - sõne
+
+string userName;
+
+userName = Console.ReadLine();
+
+Console.WriteLine("Hello" + ", " + userName + "!");
+
+//string interpolation
+
+Console.WriteLine($"Hello, {userName}!");
