d42741c226930e833cdb70ca81a31b9a8cde64eb
LaurMaldis <117661481+LaurMaldis@users.noreply.github.com>
2023-09-08 13:34:18 +0300

    week2
    week tööd

---
+++ Week-2/UserGenderValidator/UserAgeValidation/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+//rakendus küsib kasutajalt vanust
+// kui kasutaja on noorem kui 13, siis konsool ütleb, et sa oled insta jaoks liiga noor
+// muidu et tere tulemast instasse
+
+
+Console.WriteLine("Tere, palun öelge oma vanus:");
+
+string userAge = Console.ReadLine();
+
+int userAgeNum = 0;
+
+bool isAgeNumber = Int32.TryParse(userAge, out userAgeNum); //bolean on true or false
+
+Console.WriteLine($"Esialgne sisend on {isAgeNumber}. Isik on {userAgeNum} aastat vana.");
+
+if (isAgeNumber)
+{
+    if (userAgeNum < 13)
+    {
+        Console.WriteLine($"Te olete {userAgeNum} ning ei tohi instagrammi kasutada!");
+    }
+    else
+    {
+        Console.WriteLine("Tere tulemast Instagrammi!");
+    }
+}
+else
+{
+    Console.WriteLine("Ei suutnud teie vanust lugeda.");
+};
+
+//int userAge = Int32.Parse(Console.ReadLine());
+//if (userAge < 13)
+//{
+//    Console.WriteLine($"Te olete {userAge} ning ei tohi instagrammi kasutada!");
+//}
+//else if (userAge => 13)
+//{
+//    Console.WriteLine("Tere tulemast Instagrammi!");
+//};
---
+++ Week-2/UserGenderValidator/UserGenderValidator/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//rakendus küsib kasutajalt valida tema sugu (n/m)
+//rakendus küsib kasutajalt sisestada perekonnanime
+// valikute järel teeb vastava väljundi
+//"Welcome, Mr. [sisend]
+
+Console.WriteLine("Valige oma sugu (n/m):");
+
+char userGender = Char.Parse(Console.ReadLine()); // see rida võtab sisendi
+
+Console.WriteLine("Palun öelge oma nimi:");
+string userLName = Console.ReadLine(); //
+
+if (userGender == 'm')
+{
+    Console.WriteLine($"Tere tulemast, hr. {userLName}");
+} 
+else if (userGender == 'n')
+{
+    Console.WriteLine($"Tere tulemast, pr. {userLName}");
+}
+else
+{
+    Console.WriteLine($"Palun sisestage õige sugu, {userLName}!");
+};
+
+
+
+//Console.WriteLine($"Tere tulemast {userGender} {userLName}!");
