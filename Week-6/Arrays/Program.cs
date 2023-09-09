674a2ea23c0e5ee265cefd501b436374111fbeb0
LaurMaldis <117661481+LaurMaldis@users.noreply.github.com>
2023-09-09 22:21:45 +0300

    Week-6 sõned ja massiivid
    Sõned ja Massiivid

---
+++ Week-6/Arrays/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+string[] fruit = new string[3];
+fruit[0] = "ounad";
+fruit[1] = "banaanid";
+fruit[2] = "apelsinid";
+Console.WriteLine($"{fruit[0]}, {fruit[1]}, {fruit[2]}");
+
+int fruitArrayLength = fruit.Length;
+
+Console.WriteLine($"Su massiivis on {fruitArrayLength} elementi");
+
+
+for (int i = 0; i < fruitArrayLength; i++)
+{
+    fruit[i] = fruit[i].Replace(fruit[i][0], char.ToUpper(fruit[i][0]));
+}
+
+
+foreach (string element in fruit)
+{
+    Console.WriteLine(element);
+}
---
+++ Week-6/RandomEle/Program.cs
@@ -0,0 +1,16 @@
+﻿
+string[] snacks = { "šoko", "diiz nuts", "kartul", "kapsas", "kana", "porgand" };
+
+Random rnd = new Random();
+
+int randomIndex = rnd.Next(0, snacks.Length);
+
+Console.WriteLine($"Täna me sööme {snacks[randomIndex]}.");
+
+
+string[] colors = { "red", "green", "blue", "pink", "yellow" };
+
+for (int i = colors - 1; i > 0; i--)
+{
+    Console.WriteLine(colors[i]);
+}
